9efde6609fc496ed1ec14e04bb9fa15a09f6f410
bndr-966 <alyamibandr@gmail.com>
2025-06-24 12:05:49 +0300

    new file:   average.py calculate gread average for 6 student

---
+++ average.py
@@ -0,0 +1,22 @@
+student=1
+sum=0
+while student<=6:
+    grade=int(input( "enter grade :"))
+    
+    student+=1
+    sum=sum + grade
+          
+            ##             
+
+print(sum/6)
+
+student = 1
+total = 0
+
+while student <= 6:
+    grade = int(input("Enter grade: "))  # Convert input to integer
+    total += grade
+    student += 1
+
+average = total / 6
+print("Average grade:", average)
